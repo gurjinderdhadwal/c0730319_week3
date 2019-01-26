d8c17c18449d96fe964814e2a72a850b6afedd65
visualstudio <visualstudio@122-PC24>
2019-01-25 21:36:40 -0500

    final commit

--- New folder/c0730319/c0730319/Program.cs
+++ New folder/c0730319/c0730319/Program.cs
@@ -11,18 +11,55 @@ namespace c0730319
         static void Main(string[] args)
         {
         }
-    }
-    class countryside
-    {
+        class Countryside
+        {
+            Village Alst;
+            Village Maeland;
+            Village Schenig;
+
+            public void InitializeMap()
+            {
+                Alst = new Village();
+                Alst.VillageName = "Alst";
+                Maeland = new Village();
+                Maeland.VillageName = "Maeland";
+                Maeland.isAstrildeHere = true;
+                Schenig = new Village();
+                Schenig.VillageName = "Schenig";
+
+                Alst.NextVillage = Maeland;
+                Maeland.NextVillage = Schenig;
+                Schenig.NextVillage = Alst;
+            }
+
+            public void FindAstrilde()
+            {
+                Village CurrentVillage = this.Maeland;
+                if (CurrentVillage.isAstrildeHere)
+                {
+                    Console.WriteLine("Hugi found Astrilde!! ");
+                }
+                Village NextVillage;
+
+                NextVillage = CurrentVillage.NextVillage;
+            }
+
+
+
+
+        }
+
+        class Village
+        {
+            public Village PreviousVillage;
+            public Village NextVillage;
+            public String VillageName;
+            public bool isAstrildeHere = false;
+            public Village() { }
+        }
+
+
 
     }
 
-    class village
-    {
-        Node previousvillage;
-        Node Nextvillage;
-        String villageName;
-        bool isAstrildehere = false;
-        public village(){}
-    }
 }
