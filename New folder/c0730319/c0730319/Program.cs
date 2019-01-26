0eb465719fe7fef049a4e9da71338251711bc833
visualstudio <visualstudio@122-PC24>
2019-01-25 19:43:37 -0500

    node

---
+++ New folder/c0730319/c0730319/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0730319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Node
+    {
+        Node previousNode;
+
+        Node Nextnode;
+    }
+}
