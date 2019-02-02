fab2f15bac9fa8ca72c654e77b396ec0cf3fe515
visualstudio <visualstudio@122-PC24>
2019-02-01 21:31:22 -0500

    lastcommit

--- New folder/c0730319/c0730319/Program.cs
+++ New folder/c0730319/c0730319/Program.cs
@@ -32,6 +32,48 @@ namespace c0730319
                 Schenig.NextVillage = Alst;
             }
 
+            public void WalkAround()
+            {
+                Village CurrentVillage = this.Maeland;
+                Village NextVillage;
+
+                NextVillage = CurrentVillage.NextVillage;
+
+
+            }
+        }
+
+        class Village
+        {
+            public Village PreviousVillage;
+            public Village NextVillage;
+            public String VillageName;
+            public bool isAstrildeHere = false;
+            public Village() { }
+        }
+
+9:08 PM
+class Countryside
+        {
+            Village Alst;
+            Village Maeland;
+            Village Schenig;
+
+            public void InitializeMap()
+            {
+                Alst = new Village();
+                Alst.VillageName = "Alst";
+                Maeland = new Village();
+                Maeland.VillageName = "Maeland";
+                Maeland.isAstrildeHere = true;
+                Schenig = new Village();
+                Schenig.VillageName = "Schenig";
+
+                Alst.NextVillage = Maeland;
+                Maeland.NextVillage = Schenig;
+                Schenig.NextVillage = Alst;
+            }
+
             public void FindAstrilde()
             {
                 Village CurrentVillage = this.Maeland;
---
+++ weeek4/730319/730319/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _730319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class village
+    {
+        bool isAstrildeHere;
+        public village nextvillage;
+        public village prevvillage;
+        public string villagename;
+    }
+    class couyntryside
+    {
+        village toronto, Maple, Ajax;
+        public void run()
+        {
+            Maple = new village();
+            Maple.villagename = "toronto";
+            Maple.nextvillage = toronto;
+
+        }
+     }
+}
